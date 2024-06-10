9a259c8a7c4c7d8e50f9b83b8b3d21822b286f91
JungWanSEO <sjwan0326@gmail.com>
2024-06-10 19:08:30 +0900

    feat: webflux의 swagger추가
    - webflux로 swagger를 적용
    접속법 : http://localhost:8081/webjars/swagger-ui/index.html

--- backend/src/main/java/com/github/backend/controller/TestController.java
+++ backend/src/main/java/com/github/backend/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.github.backend.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/test")
+@Tag(name = "test API", description = "test")
 public class TestController {
 
     @Operation(summary = "테스트", description = "테스트를 해본다.")
